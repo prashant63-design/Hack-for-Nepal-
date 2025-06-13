4102c240c4614b807ae571cb9663a7b83d1d4d6e
bibek01_sys <081bct012.bibek@sagarmatha.edu.np>
2025-06-13 22:39:30 +0545

    the first ui change

---
+++ app/src/main/java/com/example/dawaidata/User.kt
@@ -0,0 +1,4 @@
+package com.example.dawaidata
+data class User{
+    val name:String
+}
--- app/src/main/java/com/example/dawaidata/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/dawaidata/login/view/LoginActivity.kt
@@ -1,16 +1,31 @@
 package com.example.dawaidata.login.view
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.AppCompatButton
 import com.example.dawaidata.R
+import com.example.dawaidata.navbar.view.NavBar
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var loginUsername: EditText
+    lateinit var loginPassword: EditText
+    lateinit var loginButton: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginUsername = findViewById(R.id.loginUsername)
+        loginPassword = findViewById(R.id.loginPassword)
+        loginButton = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, NavBar::class.java)
+            startActivity(intent)
+        }
+
 
     }
 }
