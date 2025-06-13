56d52b10dbcf0a46912fb860b14e81a25d2f4fc9
Akingkia <159263515+Akingkia@users.noreply.github.com>
2025-06-13 16:47:05 +0545

    initial commit

---
+++ app/src/main/java/com/example/dawaidata/login/view/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.dawaidata.login.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dawaidata.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
+
