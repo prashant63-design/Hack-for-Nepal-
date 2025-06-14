ba2fb28a476c1ca4d4308954a0bdc562a5a01761
Akingkia <159263515+Akingkia@users.noreply.github.com>
2025-06-14 13:05:31 +0545

    some fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -48,4 +49,12 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("com.google.mlkit:barcode-scanning:17.2.0")
+    implementation ("androidx.camera:camera-core:1.3.0")
+    implementation ("androidx.camera:camera-camera2:1.3.0")
+    implementation ("androidx.camera:camera-lifecycle:1.3.0")
+    implementation ("androidx.camera:camera-view:1.3.0")
+    implementation  ("com.journeyapps:zxing-android-embedded:4.1.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
 }
